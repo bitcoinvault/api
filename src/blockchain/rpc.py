f201409893ac638ac12462df905468c3b91fc485
PJLewandowski <63646319+PJLewandowski@users.noreply.github.com>
2020-05-08 22:55:05 +0200

    Fixes for pull request remarks.

--- src/blockchain/rpc.py
+++ src/blockchain/rpc.py
@@ -4,47 +4,22 @@ import random
 
 url = "http://user:password@bitcoind:8332"
 
-
-def execute_rpc(rpc):
-    rpc = rpc.dict()
-    response = requests.post(url, json=rpc).json()
-    return response['result']
-
-
 def get_block_hash(block_number):
     rpc = RPC("getblockhash", [block_number])
-    block_hash = execute_rpc(rpc)
-    return block_hash
-
+    return rpc.execute()
 
 def get_block(block_number, verbosity=2):
     block_hash = get_block_hash(block_number)
     rpc = RPC("getblock", [block_hash, verbosity])
-    block = execute_rpc(rpc)
-    return block
-
+    return rpc.execute()
 
 def get_block_count():
     rpc = RPC("getblockcount")
-    return execute_rpc(rpc)
-
+    return rpc.execute()
 
 def get_hashrate():
     rpc = RPC("getnetworkhashps")
-    return execute_rpc(rpc)
-
-
-def save_json(in_dict, name):
-    dict_json = json.dumps(in_dict)
-    with open(name, "w") as f:
-        f.write(dict_json)
-
-
-def load_file(name):
-    with open(name, 'r') as f:
-        res = json.load(f)
-    return res
-
+    return rpc.execute()
 
 class RPC:
     def __init__(self, method="", params=[], rpc_id=random.random()):
@@ -52,6 +27,10 @@ class RPC:
         self.params = params
         self.jsonrpc = "2.0"
         self.id = rpc_id
+
+    def execute(self):
+        response = requests.post(url, json=self.dict()).json()
+        return response['result']
 
     def dict(self):
         payload = {
